f6a72fa2fb1af2a536ae5127aac5231cb4be5ee8
Hoanqnqu <nhathoangthuychaub1@gmail.com>
2023-04-16 00:12:25 +0700

    row

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ const Home = ({
 }: Props) => {
     console.log(netflixOriginals);
     return (
-        <div className="min-h-screen relative bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+        <div className="min-h-screen relative bg-gradient-to-b lg:h-[140vh]">
             <Head>
                 <title>Netflix</title>
             </Head>
@@ -38,7 +38,7 @@ const Home = ({
             <Header />
             <main className="relative pl-4 pb-24 lg:space-x-24 lg:pl-16">
                 <Banner netflixOriginals={netflixOriginals} />
-                <section>
+                <section className='md:space-y-24'>
                     <Row title="Trending Now" movies={trendingNow} />
                     <Row title="Top Rated" movies={topRated} />
                     <Row title="Action Thrillers" movies={actionMovies} />
