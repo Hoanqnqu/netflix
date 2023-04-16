f06681a88d703ef82885bce7eafb765aff04fd96
Hoanqnqu <nhathoangthuychaub1@gmail.com>
2023-04-16 18:43:51 +0700

    fix ko dc=)))

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -16,10 +16,10 @@ const Banner = ({ netflixOriginals }: Props) => {
     useEffect(() => {
         setMovie(netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]);
     }, [netflixOriginals]);
-  
+
     return (
         <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
-            <div className=" absolute top-0 left-0 h-[95vh] -z-10 w-[99.6vw]">
+            <div className=" absolute top-0 left-0 h-[95vh] -z-10 w-screen">
                 <Image
                     src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
                     alt=""
